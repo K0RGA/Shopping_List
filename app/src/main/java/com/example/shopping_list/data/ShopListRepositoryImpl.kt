1664fe86817d2e7db5abd9cecdaa66f163569e11
K0RGA <nikita-gon1@mail.ru>
2022-02-28 23:12:02 +0300

    Add ShopListRepositoryImpl

---
+++ app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.example.shopping_list.data
+
+import com.example.shopping_list.domain.ShopItem
+import com.example.shopping_list.domain.ShopListRepository
+
+object ShopListRepositoryImpl: ShopListRepository {
+    override fun addShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteShopItem(shopItemId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        TODO("Not yet implemented")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        TODO("Not yet implemented")
+    }
+}
