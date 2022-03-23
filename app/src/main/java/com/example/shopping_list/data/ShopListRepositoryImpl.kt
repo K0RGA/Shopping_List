202e845f36ea7d55df45290f42e892170de7dd93
K0RGA <nikita-gon1@mail.ru>
2022-03-23 21:46:12 +0300

    Add Implementation of Business Logic

--- app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
@@ -4,23 +4,32 @@ import com.example.shopping_list.domain.ShopItem
 import com.example.shopping_list.domain.ShopListRepository
 
 object ShopListRepositoryImpl: ShopListRepository {
+
+    private var shopList = mutableListOf<ShopItem>()
+
+    var autoIncrementId = 0
+
     override fun addShopItem(shopItem: ShopItem) {
-        TODO("Not yet implemented")
+        if (shopItem.id == ShopItem.UNDEFINED_ID) shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
     }
 
-    override fun deleteShopItem(shopItemId: Int) {
-        TODO("Not yet implemented")
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
     }
 
     override fun editShopItem(shopItem: ShopItem) {
-        TODO("Not yet implemented")
+        val oldShopItem = getShopItem(shopItem.id)
+        shopList.remove(oldShopItem)
+        addShopItem(shopItem)
     }
 
+    //если что убрать !!, а то хуйня поулчается
     override fun getShopItem(shopItemId: Int): ShopItem {
-        TODO("Not yet implemented")
+        return shopList.find { it.id == shopItemId }!!
     }
 
     override fun getShopList(): List<ShopItem> {
-        TODO("Not yet implemented")
+        return shopList.toMutableList()
     }
 }
--- app/src/main/java/com/example/shopping_list/domain/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/example/shopping_list/domain/DeleteShopItemUseCase.kt
@@ -2,7 +2,7 @@ package com.example.shopping_list.domain
 
 class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun deleteShopItem(shopItemId: Int){
-        shopListRepository.deleteShopItem(shopItemId)
+    fun deleteShopItem(shopItem: ShopItem){
+        shopListRepository.deleteShopItem(shopItem)
     }
 }
