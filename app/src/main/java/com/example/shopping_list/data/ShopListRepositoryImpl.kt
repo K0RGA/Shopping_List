a0c289ef2aa0b733ba64282008a298e0ec01ca0e
K0RGA <nikita-gon1@mail.ru>
2022-03-23 22:26:33 +0300

    Add MainViewModel

--- app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopping_list/data/ShopListRepositoryImpl.kt
@@ -9,6 +9,14 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     var autoIncrementId = 0
 
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("$i", i, true)
+            addShopItem(item)
+        }
+
+    }
+
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) shopItem.id = autoIncrementId++
         shopList.add(shopItem)
---
+++ app/src/main/java/com/example/shopping_list/presentation/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.shopping_list.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shopping_list.data.ShopListRepositoryImpl
+import com.example.shopping_list.data.ShopListRepositoryImpl.getShopItem
+import com.example.shopping_list.domain.DeleteShopItemUseCase
+import com.example.shopping_list.domain.EditShopItemUseCase
+import com.example.shopping_list.domain.GetShopListUseCase
+import com.example.shopping_list.domain.ShopItem
+
+class MainViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+}
